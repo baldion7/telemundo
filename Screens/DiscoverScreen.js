64324c585bedd6bf8310b468b0bef4aa432979f3
baldion7 <73363421+baldion7@users.noreply.github.com>
2022-11-10 19:49:10 -0500

    Add files via upload

---
+++ Screens/DiscoverScreen.js
@@ -0,0 +1,116 @@
+import React, { useContext } from "react";
+import {Dimensions,Image,StyleSheet,Text,TouchableOpacity,View,} from "react-native";
+import Carousel from "react-native-snap-carousel";
+import { categories, sources } from "../API/api";
+import { NewsContext } from "../API/Context";
+import Search from "../components/Search";
+
+const DiscoverScreen = () => {
+  const windowWidth = Dimensions.get("window").width;
+  const SLIDE_WIDTH = Math.round(windowWidth / 3.5);
+
+  const { setCategory, setSource, darkTheme } = useContext(NewsContext);
+
+  return (
+    <View style={styles.discover}>
+      <Search />
+      <Text
+        style={{ ...styles.subtitle, color: darkTheme ? "white" : "black" }}
+      >
+        Categorias
+      </Text>
+      <Carousel
+        layout={"default"}
+        data={categories}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => setCategory(item.name)}
+              style={styles.category}
+            >
+              <Image source={{ uri: item.pic }} style={styles.categoryImage} />
+              <Text
+                style={{ ...styles.nombre, color: darkTheme ? "white" : "black" }}
+              >
+                {item.nombre}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
+        sliderWidth={windowWidth}
+        itemWidth={SLIDE_WIDTH}
+        activeSlideAlignment={"start"}
+        inactiveSlideScale={1}
+        inactiveSlideOpacity={1}
+      />
+      <Text
+        style={{ ...styles.subtitle, color: darkTheme ? "white" : "black" }}
+      >
+       Fuentes 
+      </Text>
+      <View style={styles.sources}>
+        {sources.map((s) => (
+          <TouchableOpacity
+            onPress={() => setSource(s.id)}
+            key={s.id}
+            style={styles.sourceContainer}
+          >
+            <Image source={{ uri: s.pic }} style={styles.sourceImage} />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default DiscoverScreen;
+
+const styles = StyleSheet.create({
+  discover: {
+    padding: 10,
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingBottom: 8,
+    marginHorizontal: 5,
+    borderBottomColor: "#007FFF",
+    borderBottomWidth: 5,
+    alignSelf: "flex-start",
+    borderRadius: 10,
+  },
+  category: {
+    height: 130,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  categoryImage: {
+    height: "60%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+  nombre: {
+    fontSize: 12.5,
+    textTransform: "capitalize",
+  },
+  sources: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    paddingVertical: 15,
+  },
+  sourceContainer: {
+    height: 150,
+    width: "40%",
+    borderRadius: 10,
+    margin: 15,
+    backgroundColor: "#cc313d",
+  },
+  sourceImage: {
+    height: "100%",
+    borderRadius: 10,
+    resizeMode: "cover",
+  },
+});
